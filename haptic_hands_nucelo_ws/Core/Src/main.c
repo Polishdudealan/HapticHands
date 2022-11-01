0cf6bcbcbc2f6ed42d006944c494c8542380152e
Polishdudealan <atondryk@umich.edu>
2022-11-01 19:53:37 -0400

    Added backup gpio pins and leds to ioc

--- haptic_hands_nucelo_ws/Core/Src/main.c
+++ haptic_hands_nucelo_ws/Core/Src/main.c
@@ -635,11 +635,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, Debug_LED_0_Pin|Debug_LED_1_Pin|Backup_GPIO_1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, vib4_Pin|vib3_Pin|vib2_Pin|vib1_Pin
-                          |vib0_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, Backup_GPIO_0_Pin|vib4_Pin|vib3_Pin|vib2_Pin
+                          |vib1_Pin|vib0_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -647,21 +650,28 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : Debug_LED_0_Pin Debug_LED_1_Pin Backup_GPIO_1_Pin */
+  GPIO_InitStruct.Pin = Debug_LED_0_Pin|Debug_LED_1_Pin|Backup_GPIO_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : Backup_GPIO_0_Pin vib4_Pin vib3_Pin vib2_Pin
+                           vib1_Pin vib0_Pin */
+  GPIO_InitStruct.Pin = Backup_GPIO_0_Pin|vib4_Pin|vib3_Pin|vib2_Pin
+                          |vib1_Pin|vib0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PB13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : vib4_Pin vib3_Pin vib2_Pin vib1_Pin
-                           vib0_Pin */
-  GPIO_InitStruct.Pin = vib4_Pin|vib3_Pin|vib2_Pin|vib1_Pin
-                          |vib0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
