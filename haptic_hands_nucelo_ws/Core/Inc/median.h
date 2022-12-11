d59a8258e7c7829ca44002a16a07605fc4082479
10RE <lorenlin@umich.edu>
2022-12-11 02:23:12 -0500

    fix-fix multi declaration in median

--- haptic_hands_nucelo_ws/Core/Inc/median.h
+++ haptic_hands_nucelo_ws/Core/Inc/median.h
@@ -11,14 +11,6 @@
 #ifndef INC_MEDIAN_H_
 #define INC_MEDIAN_H_
 
-#define ARRAY_SIZE 5
-
-typedef struct
-{
-	uint16_t data[ARRAY_SIZE];
-	uint16_t hist_data[ARRAY_SIZE];
-} median;
-
 void updateMedianArray(uint8_t finger, uint16_t val);
 
 uint16_t getMedian(uint8_t finger);
